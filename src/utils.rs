4e451ea1b0fdbddb3a7bb0a92700e7079d0bd5fb
FrauH0lle <roland.goers@posteo.de>
2024-09-24 23:18:38 +0200

    doc: Add documentation to utils module

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,8 @@
+//! This module provides various utility functions needed throughout dotdeploy.
+//!
+//! These include file operations like copy or link, manipulating file metadata and permissions as
+//! well as elevating privileges.
+
 pub(crate) mod common;
 pub(crate) mod file_checksum;
 pub(crate) mod file_fs;
