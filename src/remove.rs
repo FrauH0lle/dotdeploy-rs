2354605f452ae8c5b976e31fb2c1092022c6d6cc
FrauH0lle <roland.goers@posteo.de>
2024-09-25 16:22:13 +0200

    doc: Update remove.rs

--- src/remove.rs
+++ src/remove.rs
@@ -1,68 +1,95 @@
+//! This module handles the removal process for files and packages, including backup restoration and
+//! cleanup operations.
+
 use anyhow::{bail, Context, Result};
 use std::collections::{BTreeMap, VecDeque};
 use std::sync::Arc;
 
 use crate::utils::file_fs;
 
+/// Removes a file and restores its backup if available.
+///
+/// This function deletes the specified file and attempts to restore its backup from either the user
+/// or system store.
+///
+/// # Arguments
+///
+/// * `file` - The path of the file to remove
+/// * `stores` - Arc-wrapped tuple of database stores (user and optional system store)
+///
+/// # Returns
+///
+/// A Result indicating success or failure of the file removal and backup restoration process
 async fn remove_file<S: AsRef<str>>(
     file: S,
     stores: Arc<(crate::store::db::Store, Option<crate::store::db::Store>)>,
 ) -> Result<()> {
     if file_fs::check_file_exists(file.as_ref()).await? {
-        // Delete file
+        // Delete the file
         file_fs::delete_file(file.as_ref()).await?;
         debug!("Removed {:?}", file.as_ref());
 
-        // Check if we have a backup in the user store
+        // Check for and restore backup from the user store
         if stores
             .0
             .check_backup_exists(file.as_ref())
             .await
             .map_err(|e| e.into_anyhow())?
         {
-            // Restore backup
             stores
                 .0
                 .restore_backup(file.as_ref(), file.as_ref())
                 .await
                 .map_err(|e| e.into_anyhow())?;
-            // TODO validate restored backup
-            // Remove backup from store
+            // TODO: Implement backup validation
             stores
                 .0
                 .remove_backup(file.as_ref())
                 .await
                 .map_err(|e| e.into_anyhow())?;
 
-            info!("Restored {:?} from backup", file.as_ref());
+            info!("Restored {:?} from user store backup", file.as_ref());
         }
 
-        // Check if we have a backup in the system store
+        // Check for and restore backup from the system store (if it exists)
         if let Some(sys_store) = &stores.1 {
             if sys_store
                 .check_backup_exists(file.as_ref())
                 .await
                 .map_err(|e| e.into_anyhow())?
             {
-                // Restore backup
                 sys_store
                     .restore_backup(file.as_ref(), file.as_ref())
                     .await
                     .map_err(|e| e.into_anyhow())?;
-                // TODO validate restored backup
-                // Remove backup from store
+                // TODO: Implement backup validation
                 sys_store
                     .remove_backup(file.as_ref())
                     .await
                     .map_err(|e| e.into_anyhow())?;
 
-                info!("Restored {:?} from backup", file.as_ref());
+                info!("Restored {:?} from system store backup", file.as_ref());
             }
         }
     }
     Ok(())
 }
 
+/// Executes the removal process for files and packages.
+///
+/// This function handles the "remove" phase, including pre-actions, package removal, file removal,
+/// and post-actions.
+///
+/// # Arguments
+///
+/// * `phases` - A BTreeMap of phase names to their corresponding Phase structs
+/// * `stores` - Arc-wrapped tuple of database stores (user and optional system store)
+/// * `files` - A vector of StoreFile objects representing files to be removed
+/// * `dotdeploy_config` - Configuration for the deployment process
+///
+/// # Returns
+///
+/// A Result indicating success or failure of the overall removal process
 pub(crate) async fn remove(
     mut phases: BTreeMap<String, crate::phases::Phase>,
     stores: Arc<(crate::store::db::Store, Option<crate::store::db::Store>)>,
@@ -71,15 +98,16 @@ pub(crate) async fn remove(
 ) -> Result<()> {
     let phase_name = "remove";
     info!("Starting {} phase", phase_name.to_uppercase());
-    // We can consume the phases BTreeMap, thus remove the key from it and take ownership.
+
+    // Extract the "remove" phase from the phases BTreeMap
     if let Some(phase) = phases.remove(phase_name) {
-        // Extract actions
-        // Directly extract the inner fields if permissions is Some, otherwise set them to None
+        // Extract actions for pre, main, and post stages
         let (pre_actions, main_actions, post_actions) =
             phase.actions.map_or((None, None, None), |mut map| {
                 (map.remove("pre"), map.remove("main"), map.remove("post"))
             });
 
+        // Execute pre-stage actions
         if let Some(v) = pre_actions {
             if !v.is_empty() {
                 info!("Executing pre stage actions");
@@ -89,43 +117,49 @@ pub(crate) async fn remove(
             }
         }
 
+        // Handle package removal
         if let Some(packages) = phase.packages {
-            // Get default commands
+            // Prepare package removal command
             let default_cmds = crate::packages::default_cmds()?.1;
-            let mut install_cmd: VecDeque<String> = VecDeque::new();
+            let mut remove_cmd: VecDeque<String> = VecDeque::new();
+
+            // Determine the removal command based on config or default
             if let Some(cmd) = &dotdeploy_config.remove_pkg_cmd {
-                install_cmd = cmd.clone();
+                remove_cmd = cmd.clone();
             } else if let Some(cmd) = default_cmds.get(&dotdeploy_config.distribution) {
-                install_cmd = cmd.clone()
+                remove_cmd = cmd.clone()
             } else {
-                bail!("Failed to get package install command")
+                bail!("Failed to get package removal command")
             }
-            if let Some(cmd) = install_cmd.pop_front() {
-                // Add packages
+
+            // Execute package removal
+            if let Some(cmd) = remove_cmd.pop_front() {
+                // Add packages to the removal command
                 for pkg in packages.into_iter() {
-                    install_cmd.push_back(pkg);
+                    remove_cmd.push_back(pkg);
                 }
+
+                // Spawn the removal process
                 let mut cmd = tokio::process::Command::new(&cmd)
-                    .args(&install_cmd)
+                    .args(&remove_cmd)
                     .spawn()
                     .with_context(|| {
-                        format!("Failed to spawn {:?} with args: {:?}", cmd, install_cmd)
+                        format!("Failed to spawn {:?} with args: {:?}", cmd, remove_cmd)
                     })?;
 
-                if cmd.wait().await?.success() {
-                    
-                } else {
-                    bail!("Failed to execute {:?} with args: {:?}", cmd, install_cmd)
+                // Check if the removal was successful
+                if !cmd.wait().await?.success() {
+                    bail!("Failed to execute {:?} with args: {:?}", cmd, remove_cmd)
                 }
             }
         }
 
-        warn!("This shit better works...");
+        warn!("This shit better works...");  // TODO: Consider removing or rephrasing this debug message
         let mut set = tokio::task::JoinSet::new();
 
-        // Remove the file async
+        // Remove files asynchronously
         for file in files.clone() {
-            let stores_clone = Arc::clone(&stores); // Clone the Arc
+            let stores_clone = Arc::clone(&stores);
             set.spawn(async move {
                 match remove_file(&file.destination, stores_clone).await {
                     Ok(()) => Ok(()),
@@ -134,16 +168,17 @@ pub(crate) async fn remove(
             });
         }
 
+        // Wait for all file removal tasks to complete
         while let Some(res) = set.join_next().await {
             res??;
         }
 
-        // But remove the parent directories sync!
+        // Remove parent directories synchronously
         for file in files {
-            // Remove directory and parents if empty
             file_fs::delete_parents(&file.destination, false).await?;
         }
 
+        // Execute main-stage actions
         if let Some(v) = main_actions {
             if !v.is_empty() {
                 info!("Executing main stage actions");
@@ -153,6 +188,7 @@ pub(crate) async fn remove(
             }
         }
 
+        // Execute post-stage actions
         if let Some(v) = post_actions {
             if !v.is_empty() {
                 info!("Executing post stage actions");
